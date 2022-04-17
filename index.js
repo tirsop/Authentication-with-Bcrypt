43162091b455f53c28874321bbb4a1c41abce92f
Tirso Pascual <tpascualgarcia@gmail.com>
2022-04-17 17:02:54 +0900

    finished authentication model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'; //import express package
 const app = express(); // abbreviation of the code
 import User from './models/user.js';
 import bcrypt from 'bcrypt';
+import session from 'express-session';
 import mongoose from 'mongoose';
 mongoose
   .connect('mongodb://localhost:27017/50_authetication')
@@ -17,9 +18,20 @@ const __dirname = new URL('.', import.meta.url).pathname;
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs'); // for requiring ejs files.
 app.use(express.urlencoded({ extended: true })); // need this line to use req.body.  use runs a function in every single request.
+const sessionConfig = {
+  secret: 'not a good secret',
+  resave: false,
+  saveUninitialized: true,
+};
+app.use(session(sessionConfig));
+
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) return res.redirect('/login');
+  next();
+};
 
 app.get('/', (req, res) => {
-  res.send('THIS IS THE HOME PAGE');
+  res.render('home');
 });
 
 app.get('/register', (req, res) => {
@@ -28,17 +40,34 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
   const { password, username } = req.body;
-  const hash = await bcrypt.hash(password, 12);
-  const user = new User({
-    username,
-    password: hash,
-  });
+  const user = new User({ username, password });
   await user.save();
-  res.redirect('/');
+  req.session.user_id = user._id;
+  res.redirect('/secret');
 });
 
-app.get('/secret', (req, res) => {
-  res.send('This is a secret!.you cannot see unless login');
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await User.findAndValidate(username, password);
+  if (foundUser) {
+    req.session.user_id = foundUser._id;
+    res.redirect('/secret');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.post('/logout', (req, res) => {
+  req.session.user_id = null; // or  req.session.destroy();   which will delete all the information
+  res.redirect('/login');
+});
+
+app.get('/secret', requireLogin, (req, res) => {
+  res.render('secret');
 });
 
 const port = 3000;
--- models/user.js
+++ models/user.js
@@ -1,14 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, "Usurname cannot be blank"],
+    required: [true, 'Usurname cannot be blank'],
   },
   password: {
     type: String,
-    required: [true, "Password cannot be blank"],
+    required: [true, 'Password cannot be blank'],
   },
 });
 
-export default mongoose.model("User", userSchema);
+userSchema.statics.findAndValidate = async function (username, password) {
+  const foundUser = await this.findOne({ username });
+  const isValid = await bcrypt.compare(password, foundUser.password);
+  return isValid ? foundUser : false;
+};
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+export default mongoose.model('User', userSchema);
